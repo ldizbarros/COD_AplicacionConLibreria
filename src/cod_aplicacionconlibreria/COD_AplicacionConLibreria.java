8a1b09f7e1aca23cf68bdd9ddf8a61040a04cf7a
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-01-26 09:43:59 +0100

    Llamada a la libreria

--- src/cod_aplicacionconlibreria/COD_AplicacionConLibreria.java
+++ src/cod_aplicacionconlibreria/COD_AplicacionConLibreria.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cod_aplicacionconlibreria;
 
-/**
- *
- * @author ldizbarros
- */
+import libreria_cod.Libreria;
+
 public class COD_AplicacionConLibreria {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Libreria.hacerPregunta("Voy a morir joven?");
     }
     
 }
