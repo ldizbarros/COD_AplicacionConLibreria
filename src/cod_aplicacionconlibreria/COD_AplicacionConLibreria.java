69db48ea7862583316991b15d268162073dcc190
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-01-26 09:50:56 +0100

    Comentarios JavaDoc

--- src/cod_aplicacionconlibreria/COD_AplicacionConLibreria.java
+++ src/cod_aplicacionconlibreria/COD_AplicacionConLibreria.java
@@ -1,10 +1,17 @@
 package cod_aplicacionconlibreria;
 
-import libreria_cod.Libreria;
+import com.otros.Libreria.Libreria;
 
+/**
+ * Aplicacion para usar la libreria
+ * @author ldizbarros
+ */
 public class COD_AplicacionConLibreria {
 
     public static void main(String[] args) {
+        /**
+         * Llamada a una funcion de la libreria
+         */
         Libreria.hacerPregunta("Voy a morir joven?");
     }
     
